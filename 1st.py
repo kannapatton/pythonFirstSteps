f8cbc5b4baef16060900d98a4bc2dd4dac877cc9
kannapatton <kandirbrts@gmail.com>
2023-04-27 11:55:51 -0500

    whut

---
+++ 1st.py
@@ -0,0 +1,10 @@
+print('Hello from python')
+"""Python Indentation
+Indentation refers to the spaces at the beginning of a code line.
+
+Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+
+Python uses indentation to indicate a block of code."""
+if 5 > 2:
+  print("Five is greater than two!") #use 3  spaces to indent (number not really important as long as it's more than 1, BUT MUST BE THE SAME NUMBER EACH TIME)
+#   whut
